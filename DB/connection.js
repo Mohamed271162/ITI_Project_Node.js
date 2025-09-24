30befa611b8d782ea34ed14c577c3e37acac477d
mohamed ashraf <mohamed.ashraf.kebal@gmail.com>
2025-09-24 19:58:03 +0300

    habdaAshraf

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectionDB = async () => {
     return await mongoose
-        .connect(process.env.CONNECTION_DB_CLOUD)
+        .connect("mongodb+srv://mohamedashrafkebal_db_user:mohamed1234@cluster0.pukr5dh.mongodb.net/")
         .then((res) => console.log('DB connection success'))
         .catch((err) => console.log('DB connection Fail', err))
 }
